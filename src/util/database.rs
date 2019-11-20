a2866497a757b55ffbdc1318c5c27d3b136c1ce8
mulark <mulark11@gmail.com>
2019-11-19 17:25:14 -0700

    this

--- src/util/database.rs
+++ src/util/database.rs
@@ -11,7 +11,56 @@ use rusqlite::{Connection, NO_PARAMS};
 use rusqlite::params;
 use std::fs;
 use std::fs::OpenOptions;
-mod create_sql;
+
+pub const CREATE_SQL: &str = "
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS `collection` (
+  `collection_id` integer NOT NULL PRIMARY KEY AUTOINCREMENT
+,  `factorio_version` varchar(10)  NOT NULL
+,  `platform` varchar(100)  NOT NULL
+,  `executable_type` varchar(100)  NOT NULL
+,  `cpuid` text NULL
+);
+
+CREATE TABLE IF NOT EXISTS `benchmark` (
+  `benchmark_id` integer NOT NULL PRIMARY KEY AUTOINCREMENT
+,  `map_name` varchar(100)  NOT NULL
+,  `runs` integer  NOT NULL CHECK (`runs` > 0)
+,  `ticks` integer  NOT NULL
+,  `map_hash` char(64)  NOT NULL
+,  `collection_id` integer  NOT NULL
+,  CONSTRAINT `benchmark_base_ibfk_1` FOREIGN KEY (`collection_id`) REFERENCES `collection` (`collection_id`)
+,  CONSTRAINT `hash_length_check` CHECK (length(`map_hash`) = 64)
+);
+CREATE INDEX IF NOT EXISTS 'idx_benchmark_base_collection_id' ON 'benchmark' (`collection_id`);
+
+CREATE TABLE IF NOT EXISTS `verbose` (
+'unused_row_index' integer PRIMARY KEY AUTOINCREMENT
+,  `run_index` integer NOT NULL
+,  `tick_number` integer NOT NULL
+,  `wholeUpdate` integer  NOT NULL
+,  `gameUpdate` integer  NOT NULL
+,  `circuitNetworkUpdate` integer   NOT NULL
+,  `transportLinesUpdate` integer   NOT NULL
+,  `fluidsUpdate` integer   NOT NULL
+,  `entityUpdate` integer   NOT NULL
+,  `mapGenerator` integer   NOT NULL
+,  `electricNetworkUpdate` integer   NOT NULL
+,  `logisticManagerUpdate` integer   NOT NULL
+,  `constructionManagerUpdate` integer   NOT NULL
+,  `pathFinder` integer   NOT NULL
+,  `trains` integer   NOT NULL
+,  `trainPathFinder` integer   NOT NULL
+,  `commander` integer   NOT NULL
+,  `chartRefresh` integer   NOT NULL
+,  `luaGarbageIncremental` integer   NOT NULL
+,  `chartUpdate` integer   NOT NULL
+,  `scriptUpdate` integer   NOT NULL
+,  `benchmark_id` integer  NOT NULL
+,  CONSTRAINT `benchmark_verbose_ibfk_1` FOREIGN KEY (`benchmark_id`) REFERENCES `benchmark` (`benchmark_id`)
+);
+COMMIT;
+";
 
 lazy_static! {
     static ref DB_CONNECTION: Mutex<Connection> = Mutex::new(setup_database(false));
@@ -124,7 +173,7 @@ pub fn upload_verbose(verbose_data: Vec<String>, benchmark_id: u32, collection_i
 }
 
 fn create_tables_in_db(database: &Connection) {
-    match database.execute_batch(create_sql::CREATE_SQL) {
+    match database.execute_batch(CREATE_SQL) {
         Ok(_) => (),
         Err(e) => {
             eprintln!("Couldn't create sql in db?");
